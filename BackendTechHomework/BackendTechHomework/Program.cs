80c1a80b1203e22d11877ecbba1f20824e9a4fd2
Davide Losco <davide.losco@avanade.com>
2021-10-14 13:12:03 +0200

    [ADD] Create solution

---
+++ BackendTechHomework/BackendTechHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackendTechHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
