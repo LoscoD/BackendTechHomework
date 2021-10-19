20d59a1b363bc522bbeab746b208cf8c576838d9
Davide Losco <davide.losco@avanade.com>
2021-10-19 12:11:54 +0200

    [UPDATE] refactored code: removed unused classes

--- BackendTechHomework/BackendTechHomework/Models/ForecastDay.cs
+++ BackendTechHomework/BackendTechHomework/Models/ForecastDay.cs
@@ -11,7 +11,5 @@ namespace BackendTechHomework.Models
         public string date { get; set; }
         public int date_epoch { get; set; }
         public Day day { get; set; }
-        public Astro astro { get; set; }
-        public List<Hour> hour { get; set; }
     }
 }
--- BackendTechHomework/BackendTechHomework/Models/Weather.cs
+++ BackendTechHomework/BackendTechHomework/Models/Weather.cs
@@ -8,8 +8,6 @@ namespace BackendTechHomework.Models
 {
     class Weather
     {
-        public Location location { get; set; }
-        public Current current { get; set; }
         public Forecast forecast { get; set; }
     }
 }
